5997d34083a7e9d6a65f556d4542fca68eba4b81
Service Web <sw@gssmail.com>
2024-03-14 10:06:06 -0500

    Commit changes in C:\Websites\Git\emarlow\5382\GCG_5382_PART_MARKS.sql

---
+++ GCG_5382_PART_MARKS.sql
@@ -0,0 +1,7 @@
+/* ==========================================
+ * TABLE: GCG_5382_PART_MARKS
+ * ========================================== */
+CREATE TABLE "GCG_5382_PART_MARKS"(
+ "PART" CHAR(20) NOT NULL ,
+ "PART_MARKINGS_LIST" NVARCHAR(4000));
+
