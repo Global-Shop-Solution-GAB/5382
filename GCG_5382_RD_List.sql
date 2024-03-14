19cf4a84c8990dd44ab2247be4aeb85f3a898895
Service Web <sw@gssmail.com>
2024-03-14 10:06:06 -0500

    Commit changes in C:\Websites\Git\emarlow\5382\GCG_5382_RD_List.sql

---
+++ GCG_5382_RD_List.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_5382_RD_List
+ * ========================================== */
+CREATE TABLE "GCG_5382_RD_List"(
+ "PART" CHAR(20) NOT NULL ,
+ "PART_RD_LIST" NVARCHAR(4000),
+ "ROUTER" CHAR(20));
+
